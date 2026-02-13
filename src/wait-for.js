1f03292850666245121014fd074b2c0b514ec860
kaspernj <kasper@diestoeckels.de>
2026-02-13 18:05:54 +0000

    Expand waitFor options JSDoc typing

--- src/wait-for.js
+++ src/wait-for.js
@@ -3,6 +3,12 @@
 import wait from "./wait.js"
 
 /**
+ * @typedef {object} WaitForOptions
+ * @property {number} [timeout] The timeout in milliseconds (default: 5000).
+ * @property {number} [wait] The wait time in milliseconds (default: 50).
+ */
+
+/**
  * Waits for a callback to run without throwing an error and retries until the timeout is reached.
  * @template T
  * @overload
@@ -13,29 +19,28 @@ import wait from "./wait.js"
  * Waits for a callback to run without throwing an error and retries until the timeout is reached.
  * @template T
  * @overload
- * @param {object} opts Options.
- * @param {number} [opts.timeout] The timeout in milliseconds (default: 5000)
- * @param {number} [opts.wait] The wait time in milliseconds (default: 50)
+ * @param {WaitForOptions} opts Options.
  * @param {() => (T | Promise<T>)} callback The callback.
  * @returns {Promise<T>} Resolves with the callback result.
  */
 /**
  * Waits for a callback to run without throwing an error and retries until the timeout is reached.
  * @template T
- * @param {object | (() => (T | Promise<T>))} opts Options or the callback when no options are provided.
+ * @param {WaitForOptions | (() => (T | Promise<T>))} opts Options or the callback when no options are provided.
  * @param {() => (T | Promise<T>)} [callback] The callback.
  * @returns {Promise<T>} Resolves with the callback result.
  */
 export default async function waitFor(opts, callback) {
-  /** @type {object | undefined} */
-  let options = opts
+  /** @type {WaitForOptions | undefined} */
+  let options
 
   /** @type {(() => (T | Promise<T>)) | undefined} */
   let resolvedCallback = callback
 
   if (typeof opts === "function") {
     resolvedCallback = opts
-    options = undefined
+  } else {
+    options = opts
   }
 
   if (resolvedCallback == undefined) throw new Error("Somehow callback is undefined")
